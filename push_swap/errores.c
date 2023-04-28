22228584bd0139b648fde25fb07c667f58fc5d3d
meryhidalgo <112578400+meryhidalgo@users.noreply.github.com>
2023-04-28 20:43:02 +0200

    Delete errores.c

--- push_swap/errores.c
+++
@@ -1,3 +0,0 @@
-//Algunos de los posibles errores son: argumentos que no son enteros,
-//argumentos superiores a un int, y/o encontrar números duplicados
-// https://medium.com/@jamierobertdawson/push-swap-the-least-amount-of-moves-with-two-stacks-d1e76a71789a
